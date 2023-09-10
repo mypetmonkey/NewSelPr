99e63b32dfc4bc2bdbb6fc46fea8056e103c0110
rajha <rajha@rajhansji.Dlink>
2023-09-10 22:31:29 +0530

    test

---
+++ src/test/java/com/demo/BookMyshow.java
@@ -0,0 +1,28 @@
+package com.demo;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BookMyshow {
+	@Test
+	public void test() throws AWTException, InterruptedException {
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver=new EdgeDriver();
+		driver.get("https://in.bookmyshow.com/");
+		Thread.sleep(6000);
+		Robot r=new Robot();
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		driver.findElement(By.xpath("//span[text()='Bengaluru']")).click();
+		
+	}
+
+}
