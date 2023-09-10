145e7cdb93da6deadd7496ab76a4e2c11d865736
rajha <rajha@rajhansji.Dlink>
2023-09-10 22:01:45 +0530

    checking

---
+++ src/test/java/com/demo/Demo.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+	
+	@Test
+	public void test() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+}
